ca7539dcba8cc373ce08b50a58cbde39c65c5164
Ivan Fabrichnov <ifabrichnov@yandex.ru>
2024-10-10 22:19:42 +0300

    Добавление страницы Профиля
    1. Разбитие на модули;
    2. Стилизация;
    3. Добавление в vite.cfg;

--- vite.config.mjs
+++ vite.config.mjs
@@ -20,6 +20,7 @@ export default defineConfig({
                 register: resolve(__dirname, 'src/pages/register.html'),
                 404: resolve(__dirname, 'src/pages/404.html'),
                 500: resolve(__dirname, 'src/pages/500.html'),
+                profile: resolve(__dirname, 'src/pages/profile.html'),
             },
         },
     }
