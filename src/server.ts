6a68d3ac2468c05a1fed02d9ff359fd40d8a9afb
Ivan Fabrichnov <ifabrichnov@yandex.ru>
2024-10-21 23:36:51 +0300

    Открытие спринта №2
    - Установка typescript;
    - Установка eslinter;
    - Установка зависимостей;
    - Сервер на ts;
    - tsconfig добавлен;
    - добавлены configs;

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('dist'));
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
